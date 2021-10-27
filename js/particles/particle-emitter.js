3605d50f262f2d5a22c4c43b98415c2c10ee348e
WellPlacedCactus <42874619+WellPlacedCactus@users.noreply.github.com>
2021-10-27 18:51:10 -0500

    Fix bugz

--- js/particles/particle-emitter.js
+++ js/particles/particle-emitter.js
@@ -5,34 +5,40 @@ import Random from '../misc/random.js';
 
 export default class ParticleEmitter {
 
-	constructor(x, y, radius, velocity) {
-		this.x = x;
-		this.y = y;
+	constructor(position, radius, theta, color, velocity) {
+		this.position = position;
 		this.radius = radius;
+		this.theta = theta;
+		this.color = color;
 		this.velocity = velocity;
-		this.angle = 0;
-		this.color = 0;
 		this.particleHandler = new ParticleHandler();
 		this.random = new Random();
 	}
 
-	tick(canvas) {
-		this.angle += this.velocity;
-		this.x = Math.floor(canvas.width / 2 + Math.cos(this.angle) * this.radius * 2);
-		this.y = Math.floor(canvas.height / 2 + Math.sin(this.angle) * this.radius);
+	addParticles() {
+		for (let i = 0; i < 1; i++) {
+			this.particleHandler.add(new Particle(
+				[
+					this.position[0] + Math.cos(this.theta) * this.radius,
+					this.position[1] + Math.sin(this.theta) * this.radius,
+				],
+				Math.random() * Math.PI,
+				25,
+				[Math.random() * Math.PI * 2, Math.random() * 2],
+				this.color,
+				1
+			));
+		}
+	}
+
+	tick() {
+		this.theta += this.velocity;
 		this.color += 1;
-		this.particleHandler.add(new Particle(
-			[this.x, this.y],
-			Math.random() * Math.PI,
-			25,
-			[Math.random() * Math.PI * 2, Math.random() * 2],
-			this.color,
-			1
-		));
+		this.addParticles();
 		this.particleHandler.tick();
 	}
 
 	draw(c) {
 		this.particleHandler.draw(c);
 	}
-}
+};
--- js/rewrite.js
+++ js/rewrite.js
@@ -3,7 +3,7 @@
 
 import ParticleEmitter from './particles/particle-emitter.js';
 
-/////////////////////////////////////////////////////// GLOBALS
+/////////////////////////////////////////////////////// ENGINE
 
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
@@ -13,6 +13,8 @@ const c = canvas.getContext('2d');
 addEventListener('load', () => {
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
+	init();
+	loop();
 });
 
 addEventListener('resize', () => {
@@ -23,11 +25,28 @@ addEventListener('resize', () => {
 
 /////////////////////////////////////////////////////// IMPLEMENTATION
 
-let fillAlpha = 0.5;
-let particleEmitter = null;
+let fillAlpha = 0.1;
+let emitters = [];
+let emitterCount = 3;
 
 function init() {
-	particleEmitter = new ParticleEmitter(0, 0, 100, 0.1);
+	emitters = [];
+	for (let i = 0; i < emitterCount; i++) {
+		emitters.push(new ParticleEmitter(
+			[canvas.width / 2, canvas.height / 2],
+			canvas.width / 5,
+			Math.PI * 2 * i / emitterCount,
+			360 * i / emitterCount,
+			0.05
+		));
+	}
+	emitters.push(new ParticleEmitter(
+		[canvas.width / 2, canvas.height / 2],
+		0,
+		0,
+		0,
+		0
+	));
 }
 
 function loop() {
@@ -39,9 +58,8 @@ function loop() {
 	c.fillRect(0, 0, canvas.width, canvas.height);
 
 	/////////////////////////////////////////////////// particles
-	particleEmitter.tick(canvas);
-	particleEmitter.draw(c);
+	emitters.forEach(emitter => {
+		emitter.tick();
+		emitter.draw(c);
+	});
 }
-
-init();
-loop();
